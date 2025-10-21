67ee9d9f4990b73d3b3916933780ddb1048a34de
Isaac7574 <isaacsouli99@gmail.com>
2025-10-21 16:15:38 +0000

    commit

--- src/environnements/environment.ts
+++ src/environnements/environment.ts
@@ -1,6 +1,9 @@
 export const environment = {
     production: false,
     apiUrl: 'http://192.168.11.111:8080/deliacte/v1',
+    //apiUrl: 'http://188.166.86.36:8080/deliacte/v1',
     //apiUrl: 'http://localhost:8080/alfresco',
-   mockApiUrl: 'http://192.168.11.111:8080'
+   //mockApiUrl: 'http://188.166.86.36:8080'
+    mockApiUrl: 'http://192.168.11.111:8080'
+
   };
